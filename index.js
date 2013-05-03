331566367604bbde08a688de641d4814238a8078
Ilya Dmitrichenko <ilya.dmitrichenko@cosm.com>
2013-05-03 09:54:26 +0200

    Add `/feeds/:feed_id/simple_history`

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var client = restify.createStringClient({
   url: 'https://api.cosm.com'
 });
 
-var makeRequest = function(feed_id, callback) {
+var makeGetFeedRequest = function(feed_id, callback) {
   var options = {
     path: '/v2/feeds/'+feed_id,
     headers: {
@@ -14,6 +14,23 @@ var makeRequest = function(feed_id, callback) {
       'Accept':'text/csv',
       'X-ApiKey': process.env['COSM_API_KEY'],
     },
+  };
+
+  client.get(options, function(err, req, res, data) {
+    assert.ifError(err);
+    callback(data);
+  });
+}
+
+var makeGetFeedHistoryRequest = function(feed_id, qp, callback) {
+  var options = {
+    path: '/v2/feeds/'+feed_id,
+    headers: {
+      'Agent':'cosm-datahub-helper/0.0.1',
+      'Accept':'text/csv',
+      'X-ApiKey': process.env['COSM_API_KEY'],
+    },
+    query: qp,
   };
 
   client.get(options, function(err, req, res, data) {
@@ -32,12 +49,24 @@ server.use(restify.bodyParser());
 
 server.get('/feeds/:feed_id', function (req, res, next) {
   res.header('Content-Type', 'text/csv');
-  makeRequest(req.params['feed_id'], function (data) {
+  makeGetFeedRequest(req.params['feed_id'], function (data) {
     res.send("datastream,timestamp,value\n"+data);
   });
   return next();
 });
 
+server.get('/feeds/:feed_id/simple_history', function (req, res, next) {
+  var qp = {
+    start: '2013-05-01T14:00:00Z',
+    interval: 900,
+    per_page: 1000,
+  };
+  res.header('Content-Type', 'text/csv');
+  makeGetFeedHistoryRequest(req.params['feed_id'], qp, function (data) {
+    res.send("datastream,timestamp,value\n"+data);
+  });
+  return next();
+});
 
 server.listen(process.env['PORT'], function () {
   console.log('%s listening at %s', server.name, server.url);
